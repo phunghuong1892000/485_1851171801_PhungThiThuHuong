4ffa68e4923912d43efa7af136a0dc9320aa7d45
phunghuong1892000 <71805088+phunghuong1892000@users.noreply.github.com>
2020-11-12 09:53:45 +0700

    Add files via upload

---
+++ diendan.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"></head>
+    <link rel="stylesheet" href="diendan.css">
+    <title>Diễn đàn khoa CNTT</title>
+</head>
+<body>
+    <div class="banner-head">
+        <a class="community" href="news.html">Community</a>
+        <a href="news.html">Log in</a>
+        <a href="news.html">Register</a>
+    </div>
+
+    <div class="container">
+        <!-- phần đầu -->
+        <div class="header">
+            <div class="header-logo">
+                <img src="picture/logo.jpg" alt="">
+                
+            </div>
+            <div class="header-news">
+                <a href="news.html">NEWS...</a>
+                <span>ElkArte - Justalled!</span>
+            </div>
+        </div>
+
+        <!-- phần thân -->
+        <div class="body">
+            <div class="home">
+                <a href="news.html"><i class="fas fa-home"></i></a>
+            </div>
+
+            <!-- form 1-->
+            <div class="title-1">
+                <div class="title">
+                    <h5>1. CSE Forum</h5>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Sự kiện CSE Forum</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>1 Ports<br>1 Topics</span>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="news.html">Chào mừng đến với Diễn...</a>
+                        <span>by Ban Quản trị</span> <br>
+                        <b>Last post:</b> <span>September 15, 2020, 12:17 am</span>
+                    </div>
+                </div>
+
+                <div class="col-md-12">    
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Góp ý, kiến nghị cho CSE Forum</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Phòng truyền thông CSE Forum</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports  <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+            </div>
+            <!-- form 2-->
+            <div class="title-2">
+                <div class="title">
+                    <h5>2. Đồng hành cùng mùa thi</h5>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Thông tin tuyển sinh</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span   >6 Ports<br>3 Topics</span>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="news.html">Điểm sàn tuyển sinh...</a><span>by <a href="news.html">Admin</a></span> <br>
+                        <b>Last post:</b> <span>September 15, 2020, 12:17 am</span>
+                    </div>
+                </div>
+                
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Chia sẻ kinh nghiệm</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>1 Ports <br>1 Topics</span>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="news.html">10 sai lầm của sinh vi...</a><span> by <a href="news.html">Admin</a></span> <br>
+                        <b>Last post:</b> <span>September 15, 2020, 12:59:10 am</span>
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Tài liệu ôn thi</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports  <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+            </div>
+            <!-- form 3-->
+            <div class="title-3">
+                <div class="title">
+                    <h5>3. Hành lang lớp học</h5>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Trên giảng đường</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports<br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Ngoài giảng đường</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+            </div>
+            <!-- form 4-->
+            <div class="title-4">
+                <div class="title">
+                    <h5>4. Đào tào hướng nghiệp</h5>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Chia sẻ tài liệu bằng tiếng Việt</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports<br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Các chứng chỉ Quốc tế về CNTT, tiếng Anh</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> Chia sẻ các tài liệu bằng tiếng Anh</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+            </div>
+            <!-- form 5-->
+            <div class="title-5">
+                <div class="title">
+                    <h5>5. Các thế hệ sinh viên</h5>
+                </div>
+
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> K58</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports<br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> K59</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> K60</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <a href="news.html"><i class="far fa-comment"></i> K61</a>
+                    </div>
+                    <div class="col-md-2">
+                        <span>0 Ports <br>0 Topics</span>
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+            </div>
+            <!-- form 6-->
+            <div class="title-6">
+                <div class="title">
+                    <h5>CSE.TLU - INFO CENTER</h5>
+                </div>
+                <div class="col-md-12 col-end">
+                    <a href="news.html"><i class="fas fa-chart-pie"></i> Forum stats</a>
+                    <span>8 posts in 5 topics by 1 Members - Letest Member: <a href="">Admin</a> - Most Online Today: 1</span>
+                    <span>Latest Post: "<a href="news.html">Re: Điểm sàn tuyển sinh...
+                    </a>" (September 15, 2020, 01: 01:58 am ) - <a href="news.html">View all recent posts.</a></span>
+                </div>
+
+                <div class="col-md-12 col-end">
+                    <span><i class="fas fa-chalkboard-teacher"></i> Online now: 0 User, 1 Guest </span>
+                </div>
+
+                <div class="col-md-12 two-icon">
+                    <span><i class="far fa-comment"></i> No new Posts</span>
+                    <span><i class="fas fa-video"></i> Redirect Board</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- phần cuối -->
+    <div class="footer">
+            <span class="col-md-8">Powered by ElkArte 1.1.6 | Credits</span>
+            <i class="fas fa-wifi"></i>
+    </div>
+     <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="diendan.js"></script>
+</body>
+</html>
